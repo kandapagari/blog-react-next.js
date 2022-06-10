72a437c78f0c5ef70ca78afc7163e2b07b618716
Pavan Kumar Kandapagari <40955141+kandapagari@users.noreply.github.com>
2022-06-10 16:00:54 +0200

    feat: final almighty push

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPost } from '../services';
+import { FeaturedPosts } from '../sections';
 
 
-
-export default function Home( {posts} ) {
+export default function Home({ posts }) {
     return (
         <div className="container mx-auto px-10 mb-8">
             <Head>
                 <title>My blog</title>
                 <link rel='icon' href='/favcon.ico' />
             </Head>
+            <FeaturedPosts />
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                 <div className="lg:col-span-8 col-span-1">
                     {posts.map((post) => (
